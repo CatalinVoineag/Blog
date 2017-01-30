a38843c25230a6a3b220d9d10f2ab30406a04c40
catalin <catalin94v@gmail.com>
2017-01-30 23:39:10 +0000

    message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
 
-  :openssl_verify_mode  => 'none'
+
 
 
 config.action_mailer.smtp_settings = {
