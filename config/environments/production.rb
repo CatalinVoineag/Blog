1f38fec2bdee3c0f281514ef138f2bae243da485
catalin <catalin94v@gmail.com>
2017-01-30 23:29:16 +0000

    message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
      :address              => "smtp.gmail.com",
      :port                 => 587,
-     :user_name            => ENV['gmail_username'],
-     :password             => ENV['gmail_password'],
+     :user_name            => 'catalin94v@gmail.com',
+     :password             => 'noexcuses414',
      :authentication       => "plain",
     :enable_starttls_auto => true
 }
